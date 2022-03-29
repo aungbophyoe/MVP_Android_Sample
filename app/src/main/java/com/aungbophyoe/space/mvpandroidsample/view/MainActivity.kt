704dfbf98a60ec8cb493ca23007ba599cb2f1ec8
aungbophyoe <abbp1001@gmail.com>
2022-03-29 20:22:29 +0630

    wip

--- app/src/main/java/com/aungbophyoe/space/mvpandroidsample/view/MainActivity.kt
+++ app/src/main/java/com/aungbophyoe/space/mvpandroidsample/view/MainActivity.kt
@@ -7,6 +7,7 @@ import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.aungbophyoe.space.mvpandroidsample.R
+import com.aungbophyoe.space.mvpandroidsample.adapter.UserRecyclerAdapter
 import com.aungbophyoe.space.mvpandroidsample.model.User
 import com.aungbophyoe.space.mvpandroidsample.mvp.presenter.UserPresenter
 import com.aungbophyoe.space.mvpandroidsample.mvp.presenter.UserPresenterImpl
@@ -15,16 +16,26 @@ import kotlinx.coroutines.withContext
 
 class MainActivity : AppCompatActivity(),UserView {
     private var userPresenter: UserPresenter? = null
+    private val userRecyclerAdapter by lazy {
+        UserRecyclerAdapter(this)
+    }
+    private val recyclerView: RecyclerView by lazy {
+        findViewById(R.id.rvUser)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        recyclerView.layoutManager = LinearLayoutManager(this,LinearLayoutManager.VERTICAL,false)
+        recyclerView.adapter = userRecyclerAdapter
         userPresenter = UserPresenterImpl(this)
         userPresenter?.loadData()
     }
 
     override fun displayView(users: List<User>?) {
         Log.d("msg","success ${users?.size}")
+        userRecyclerAdapter.submitList(users)
+        recyclerView.adapter?.notifyDataSetChanged()
     }
 
     override fun displayError(msg: String?) {
